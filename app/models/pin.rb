33f844598494294adf615f6198d98ed7bf3dd795
Carla Vazquez <krla333@gmail.com>
2013-02-27 15:10:00 -0800

    Add user and pin association

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,5 +2,8 @@ class Pin < ActiveRecord::Base
   attr_accessible :descriptopn
   
   validates :descriptopn, presence: true
+  
+  belongs_to :user
+  validates :user_id, presence: true
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130227190829) do
+ActiveRecord::Schema.define(:version => 20130227214742) do
 
   create_table "pins", :force => true do |t|
     t.string   "descriptopn"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "user_id"
   end
+
+  add_index "pins", ["user_id"], :name => "index_pins_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
